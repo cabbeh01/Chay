ccb192dda0abb043bee6d0c70780be10184aa2b6
Casper Henningsson <cashen0116@skynet.halmstad.se>
2020-02-11 15:33:34 +0100

    Började att överföra min design in till visual studio

---
+++ Chay/Chay/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Chay
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Node0");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Node1");
+            this.pHeader = new System.Windows.Forms.Panel();
+            this.btnMini = new System.Windows.Forms.PictureBox();
+            this.btnMaxi = new System.Windows.Forms.PictureBox();
+            this.btnClose = new System.Windows.Forms.PictureBox();
+            this.Logo = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pHeader.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMini)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMaxi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnClose)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pHeader
+            // 
+            this.pHeader.BackColor = System.Drawing.Color.DarkGray;
+            this.pHeader.Controls.Add(this.btnMini);
+            this.pHeader.Controls.Add(this.btnMaxi);
+            this.pHeader.Controls.Add(this.btnClose);
+            this.pHeader.Controls.Add(this.Logo);
+            this.pHeader.Controls.Add(this.pictureBox1);
+            this.pHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pHeader.Location = new System.Drawing.Point(0, 0);
+            this.pHeader.Name = "pHeader";
+            this.pHeader.Size = new System.Drawing.Size(800, 63);
+            this.pHeader.TabIndex = 0;
+            this.pHeader.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PHeader_MouseDown);
+            // 
+            // btnMini
+            // 
+            this.btnMini.BackColor = System.Drawing.Color.SandyBrown;
+            this.btnMini.Location = new System.Drawing.Point(655, 0);
+            this.btnMini.Name = "btnMini";
+            this.btnMini.Size = new System.Drawing.Size(45, 26);
+            this.btnMini.TabIndex = 6;
+            this.btnMini.TabStop = false;
+            this.btnMini.Click += new System.EventHandler(this.BtnMini_Click);
+            // 
+            // btnMaxi
+            // 
+            this.btnMaxi.BackColor = System.Drawing.Color.Tomato;
+            this.btnMaxi.Location = new System.Drawing.Point(699, 0);
+            this.btnMaxi.Name = "btnMaxi";
+            this.btnMaxi.Size = new System.Drawing.Size(45, 26);
+            this.btnMaxi.TabIndex = 5;
+            this.btnMaxi.TabStop = false;
+            this.btnMaxi.Click += new System.EventHandler(this.BtnMaxi_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.Maroon;
+            this.btnClose.Location = new System.Drawing.Point(743, 0);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(45, 26);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.TabStop = false;
+            this.btnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // Logo
+            // 
+            this.Logo.AutoSize = true;
+            this.Logo.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Logo.Location = new System.Drawing.Point(13, 13);
+            this.Logo.Name = "Logo";
+            this.Logo.Size = new System.Drawing.Size(53, 25);
+            this.Logo.TabIndex = 3;
+            this.Logo.Text = "Chay";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DimGray;
+            this.pictureBox1.Location = new System.Drawing.Point(655, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(133, 26);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // treeView1
+            // 
+            this.treeView1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.treeView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.treeView1.Location = new System.Drawing.Point(0, 44);
+            this.treeView1.Name = "treeView1";
+            treeNode7.Name = "Node0";
+            treeNode7.Text = "Node0";
+            treeNode8.Name = "Node1";
+            treeNode8.Text = "Node1";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode7,
+            treeNode8});
+            this.treeView1.Size = new System.Drawing.Size(200, 340);
+            this.treeView1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.treeView1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 63);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 387);
+            this.panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Serverar";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pHeader);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form";
+            this.Text = "Form1";
+            this.pHeader.ResumeLayout(false);
+            this.pHeader.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMini)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMaxi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnClose)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel pHeader;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Logo;
+        private System.Windows.Forms.PictureBox btnMini;
+        private System.Windows.Forms.PictureBox btnMaxi;
+        private System.Windows.Forms.PictureBox btnClose;
+    }
+}
+
---
+++ Chay/Chay/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chay
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        
+
+        public Form()
+        {
+            InitializeComponent();
+            GraphicalComponents();
+        }
+
+        private void PHeader_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        public void GraphicalComponents()
+        {
+            Logo.Font = new Font("Ranchers", 20, FontStyle.Regular);
+            Logo.ForeColor = Color.White;
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void BtnMaxi_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnMini_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
