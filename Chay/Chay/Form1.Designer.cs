12cf0eb4d65d3074f6291d069d18d7bafb7968b9
Casper Henningsson <cashen0116@skynet.halmstad.se>
2020-02-24 09:30:34 +0100

    Fixade bordern

--- Chay/Chay/Form1.Designer.cs
+++ Chay/Chay/Form1.Designer.cs
@@ -28,17 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Kalle");
-            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Lena");
-            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Klas");
-            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Serv 1", new System.Windows.Forms.TreeNode[] {
-            treeNode1,
-            treeNode2,
-            treeNode3});
-            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Serv 2");
-            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Serv 3");
-            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Serv 4");
+            System.Windows.Forms.TreeNode treeNode78 = new System.Windows.Forms.TreeNode("Kalle");
+            System.Windows.Forms.TreeNode treeNode79 = new System.Windows.Forms.TreeNode("Lena");
+            System.Windows.Forms.TreeNode treeNode80 = new System.Windows.Forms.TreeNode("Klas");
+            System.Windows.Forms.TreeNode treeNode81 = new System.Windows.Forms.TreeNode("Serv 1", new System.Windows.Forms.TreeNode[] {
+            treeNode78,
+            treeNode79,
+            treeNode80});
+            System.Windows.Forms.TreeNode treeNode82 = new System.Windows.Forms.TreeNode("Serv 2");
+            System.Windows.Forms.TreeNode treeNode83 = new System.Windows.Forms.TreeNode("Serv 3");
+            System.Windows.Forms.TreeNode treeNode84 = new System.Windows.Forms.TreeNode("Serv 4");
             this.pHeader = new System.Windows.Forms.Panel();
+            this.btnLogin = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
             this.btnMaxi = new System.Windows.Forms.PictureBox();
             this.btnMini = new System.Windows.Forms.PictureBox();
@@ -55,7 +56,6 @@
             this.button1 = new System.Windows.Forms.Button();
             this.panel4 = new System.Windows.Forms.Panel();
             this.panel5 = new System.Windows.Forms.Panel();
-            this.btnLogin = new System.Windows.Forms.Button();
             this.pHeader.SuspendLayout();
             this.panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.btnMaxi)).BeginInit();
@@ -74,12 +74,22 @@
             this.pHeader.Controls.Add(this.panel3);
             this.pHeader.Controls.Add(this.Logo);
             this.pHeader.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pHeader.Location = new System.Drawing.Point(0, 0);
+            this.pHeader.Location = new System.Drawing.Point(2, 2);
             this.pHeader.Name = "pHeader";
-            this.pHeader.Size = new System.Drawing.Size(811, 63);
+            this.pHeader.Size = new System.Drawing.Size(807, 63);
             this.pHeader.TabIndex = 0;
             this.pHeader.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PHeader_MouseDown);
             this.pHeader.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pHeader_MouseMove);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(107, 17);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 8;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // panel3
             // 
@@ -88,7 +98,7 @@
             this.panel3.Controls.Add(this.btnMini);
             this.panel3.Controls.Add(this.btnClose);
             this.panel3.Controls.Add(this.pictureBox1);
-            this.panel3.Location = new System.Drawing.Point(669, 0);
+            this.panel3.Location = new System.Drawing.Point(665, 0);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(138, 28);
             this.panel3.TabIndex = 7;
@@ -149,28 +159,28 @@
             // 
             this.treeView1.BackColor = System.Drawing.Color.LightSteelBlue;
             this.treeView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.treeView1.Location = new System.Drawing.Point(0, 59);
+            this.treeView1.Location = new System.Drawing.Point(3, 59);
             this.treeView1.Name = "treeView1";
-            treeNode1.Name = "Node4";
-            treeNode1.Text = "Kalle";
-            treeNode2.Name = "Node6";
-            treeNode2.Text = "Lena";
-            treeNode3.Name = "Node7";
-            treeNode3.Text = "Klas";
-            treeNode4.Name = "Node0";
-            treeNode4.Text = "Serv 1";
-            treeNode5.Name = "Node1";
-            treeNode5.Text = "Serv 2";
-            treeNode6.Name = "Node0";
-            treeNode6.Text = "Serv 3";
-            treeNode7.Name = "Node1";
-            treeNode7.Text = "Serv 4";
+            treeNode78.Name = "Node4";
+            treeNode78.Text = "Kalle";
+            treeNode79.Name = "Node6";
+            treeNode79.Text = "Lena";
+            treeNode80.Name = "Node7";
+            treeNode80.Text = "Klas";
+            treeNode81.Name = "Node0";
+            treeNode81.Text = "Serv 1";
+            treeNode82.Name = "Node1";
+            treeNode82.Text = "Serv 2";
+            treeNode83.Name = "Node0";
+            treeNode83.Text = "Serv 3";
+            treeNode84.Name = "Node1";
+            treeNode84.Text = "Serv 4";
             this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode4,
-            treeNode5,
-            treeNode6,
-            treeNode7});
-            this.treeView1.Size = new System.Drawing.Size(200, 324);
+            treeNode81,
+            treeNode82,
+            treeNode83,
+            treeNode84});
+            this.treeView1.Size = new System.Drawing.Size(197, 324);
             this.treeView1.TabIndex = 1;
             // 
             // panel1
@@ -179,9 +189,9 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.treeView1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel1.Location = new System.Drawing.Point(0, 63);
+            this.panel1.Location = new System.Drawing.Point(2, 65);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 386);
+            this.panel1.Size = new System.Drawing.Size(200, 382);
             this.panel1.TabIndex = 2;
             // 
             // label1
@@ -200,9 +210,9 @@
             this.panel2.Controls.Add(this.label2);
             this.panel2.Controls.Add(this.NameServer);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(200, 63);
+            this.panel2.Location = new System.Drawing.Point(202, 65);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(611, 101);
+            this.panel2.Size = new System.Drawing.Size(607, 101);
             this.panel2.TabIndex = 3;
             // 
             // label2
@@ -230,7 +240,7 @@
             this.tbxSend.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.tbxSend.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.tbxSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbxSend.Location = new System.Drawing.Point(22, 14);
+            this.tbxSend.Location = new System.Drawing.Point(20, 14);
             this.tbxSend.Multiline = true;
             this.tbxSend.Name = "tbxSend";
             this.tbxSend.Size = new System.Drawing.Size(487, 44);
@@ -245,7 +255,7 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.button1.Location = new System.Drawing.Point(515, 14);
+            this.button1.Location = new System.Drawing.Point(511, 14);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(87, 43);
             this.button1.TabIndex = 6;
@@ -254,38 +264,29 @@
             // 
             // panel4
             // 
+            this.panel4.BackColor = System.Drawing.Color.SteelBlue;
             this.panel4.Controls.Add(this.tbxSend);
             this.panel4.Controls.Add(this.button1);
             this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel4.Location = new System.Drawing.Point(200, 380);
+            this.panel4.Location = new System.Drawing.Point(202, 378);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(611, 69);
+            this.panel4.Size = new System.Drawing.Size(607, 69);
             this.panel4.TabIndex = 7;
             // 
             // panel5
             // 
             this.panel5.BackColor = System.Drawing.Color.WhiteSmoke;
             this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel5.Location = new System.Drawing.Point(200, 164);
+            this.panel5.Location = new System.Drawing.Point(202, 166);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(611, 285);
+            this.panel5.Size = new System.Drawing.Size(607, 281);
             this.panel5.TabIndex = 8;
-            // 
-            // btnLogin
-            // 
-            this.btnLogin.Location = new System.Drawing.Point(107, 17);
-            this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(75, 23);
-            this.btnLogin.TabIndex = 8;
-            this.btnLogin.Text = "Login";
-            this.btnLogin.UseVisualStyleBackColor = true;
-            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
             this.ClientSize = new System.Drawing.Size(811, 449);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel5);
@@ -294,6 +295,7 @@
             this.Controls.Add(this.pHeader);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(2);
             this.Text = "Form1";
             this.pHeader.ResumeLayout(false);
             this.pHeader.PerformLayout();
@@ -329,8 +331,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.Panel panel5;
         private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Panel panel5;
     }
 }
 
